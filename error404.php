2a1bccc2cad908d4207dfac82f84af4fb71f39f8
diequinteros <diegoquinteros02.04.97@gmail.com>
2016-07-20 21:29:05 -0600

    Agregando paginas de errores personalizados

---
+++ error404.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <?php include '../inc/styles.php' ?>
+      <link type="text/css" rel="stylesheet" href="../css/cmarca.css"  media="screen,projection"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
+    </head>
+    <body class="purple lighten-5">
+      <?php
+        include("scrud/delete_ofertacat.php");
+        ?>  
+      <!--?php include 'inc/menu.php' ?>-->
+      <br>
+      <!-- C O N T E N E D O R -->
+      <div id="contenedor" class="container red z-depth-5">  
+            <h3> Error </h3>
+      </div>
+      <!-- F I N - C O N T E N E D O R -->
+      <?php include '../inc/scripts.php' ?>
+    </body>
+  </html>
