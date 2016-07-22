d4fbf11fcd4b1ef96d52d534d90e7066c406c6f6
Omega8351 <gorge.depaz@gmail.com>
2016-07-22 08:17:29 -0600

    Menu 100% funcional

--- inc/menu2.php
+++ inc/menu2.php
@@ -6,17 +6,17 @@ if(isset($_SESSION['tipo'])){
 		$menu .= "<div class='navbar-fixed'>
 				<nav>
 					<div class='nav-wrapper blue darken-1'>
-						<a id='Logo' href='https://localhost/Rikoshop/admin/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
+						<a id='Logo' href='http://localhost/Rikoshop/admin/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
 						<a href='#' data-activates='mobile-demo' class='button-collapse'><i class='material-icons'>menu</i></a>
 						<ul class='right hide-on-med-and-down'>
-							<li><a href='https://localhost/Rikoshop/admin/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
-							<li><a href='https://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/admin/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
+							<li><a href='http://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
 						</ul>
 						<ul class='side-nav' id='mobile-demo'>
-							<li><a href='https://localhost/Rikoshop/admin/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
-							<li><a href='https://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/admin/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
+							<li><a href='http://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
 							<footer align='center'>
 								<small>
 									<p id='Cr'>Copyright ©</p>
@@ -31,19 +31,19 @@ if(isset($_SESSION['tipo'])){
 		$menu .= "<div class='navbar-fixed'>
 				<nav>
 					<div class='nav-wrapper blue darken-1'>
-						<a id='Logo' href='https://localhost/Rikoshop/public/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
+						<a id='Logo' href='http://localhost/Rikoshop/public/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
 						<a href='#' data-activates='mobile-demo' class='button-collapse'><i class='material-icons'>menu</i></a>
 						<ul class='right hide-on-med-and-down'>
-							<li><a href='https://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
-							<li><a href='https://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
-							<li><a href='https://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
+							<li><a href='http://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
+							<li><a href='http://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
 						</ul>
 						<ul class='side-nav' id='mobile-demo'>
-							<li><a href='https://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
-							<li><a href='https://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
-							<li><a href='https://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
+							<li><a href='http://localhost/Rikoshop/public/profile.php'><i class='material-icons left'>account_circle</i>".$_SESSION['nombre_usuario']."</a></li>
+							<li><a href='http://localhost/Rikoshop/public/logout.php'><i class='material-icons left'>power_settings_new</i>Cerrar Sesion</a></li>
 							<footer align='center'>
 								<small>
 									<p id='Cr'>Copyright ©</p>
@@ -59,19 +59,19 @@ else{
 	$menu .= "<div class='navbar-fixed'>
 				<nav>
 					<div class='nav-wrapper blue darken-1'>
-						<a id='Logo' href='https://localhost/Rikoshop/public/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
+						<a id='Logo' href='http://localhost/Rikoshop/public/index.php' class='brand-logo'>( ͡° ͜ʖ ͡°)</a>
 						<a href='#' data-activates='mobile-demo' class='button-collapse'><i class='material-icons'>menu</i></a>
 						<ul class='right hide-on-med-and-down'>
-							<li><a href='https://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
-							<li><a href='https://localhost/Rikoshop/public/registro.php'><i class='material-icons left'>group_add</i>Registrate</a></li>
-							<li><a href='https://localhost/Rikoshop/public/login.php'><i class='material-icons left'>account_circle</i>Iniciar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
+							<li><a href='http://localhost/Rikoshop/public/registro.php'><i class='material-icons left'>group_add</i>Registrate</a></li>
+							<li><a href='http://localhost/Rikoshop/public/login.php'><i class='material-icons left'>account_circle</i>Iniciar Sesion</a></li>
 						</ul>
 						<ul class='side-nav' id='mobile-demo'>
-							<li><a href='https://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
-							<li><a href='https://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
-							<li><a href='https://localhost/Rikoshop/public/registro.php'><i class='material-icons left'>group_add</i>Registrate</a></li>
-							<li><a href='https://localhost/Rikoshop/public/login.php'><i class='material-icons left'>account_circle</i>Iniciar Sesion</a></li>
+							<li><a href='http://localhost/Rikoshop/public/index.php'><i class='material-icons left'>home</i>Inicio</a></li>
+							<li><a href='http://localhost/Rikoshop/public/menucat.php'><i class='material-icons left'>local_grocery_store</i>Categorías</a></li>
+							<li><a href='http://localhost/Rikoshop/public/registro.php'><i class='material-icons left'>group_add</i>Registrate</a></li>
+							<li><a href='http://localhost/Rikoshop/public/login.php'><i class='material-icons left'>account_circle</i>Iniciar Sesion</a></li>
 							<footer align='center'>
 								<small>
 									<p id='Cr'>Copyright ©</p>
