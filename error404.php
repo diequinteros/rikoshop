69703cdeaddc2db80ddd5fc374b47f9e94096ce4
diequinteros <diegoquinteros02.04.97@gmail.com>
2016-07-23 21:09:47 -0600

    Paginas de error personalizadas

--- error404.php
+++ error404.php
@@ -1,21 +1,18 @@
  <!DOCTYPE html>
   <html>
     <head>
-      <?php include '../inc/styles.php' ?>
+      <?php include 'inc/styles.php' ?>
       <link type="text/css" rel="stylesheet" href="../css/cmarca.css"  media="screen,projection"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
     </head>
     <body class="purple lighten-5">
-      <?php
-        include("scrud/delete_ofertacat.php");
-        ?>  
       <!--?php include 'inc/menu.php' ?>-->
       <br>
       <!-- C O N T E N E D O R -->
       <div id="contenedor" class="container red z-depth-5">  
-            <h3> Error </h3>
+            <h1 class="center-align white-text"> Error 404: pagina no encontrada. <i class="material-icons md-60 right">error</i></h1>
       </div>
       <!-- F I N - C O N T E N E D O R -->
-      <?php include '../inc/scripts.php' ?>
+      <?php include 'inc/scripts.php' ?>
     </body>
   </html>
